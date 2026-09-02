9d5f43ed3a5e445ca910773f80d4b7ecfff802f1
jbob-coder <jbob44201@gmail.com>
2026-09-02 12:00:24 +0400

    Add v0.5.8.1 source version constants

---
+++ worldlife_v0581_patch/app/src/main/java/com/jackwilson/worldlife/BuildConfig.kt
@@ -0,0 +1,7 @@
+package com.jackwilson.worldlife
+
+internal object BuildConfig {
+    const val APPLICATION_ID = "com.jackwilson.worldlife"
+    const val VERSION_NAME = "0.5.8.1"
+    const val VERSION_CODE = 24
+}
