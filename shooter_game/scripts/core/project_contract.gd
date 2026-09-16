6c69b27decadd8cf4dd494b1ed2dddefa3e3a61a
jbob-coder <jbob44201@gmail.com>
2026-09-16 08:23:53 +0400

    Add Shooter RPG project contract

---
+++ shooter_game/scripts/core/project_contract.gd
@@ -0,0 +1,20 @@
+class_name ShooterProjectContract
+extends RefCounted
+
+const PROJECT_ID := "shooter_rpg"
+const PROJECT_DISPLAY_NAME := "Shooter RPG"
+const SCAFFOLD_VERSION := "001"
+const TARGET_ENGINE := "Godot 4.7.2-stable"
+
+const REQUIRED_INPUT_ACTIONS: PackedStringArray = [
+	"move_left",
+	"move_right",
+	"move_forward",
+	"move_back",
+	"aim",
+	"fire",
+	"reload",
+	"dodge",
+	"interact",
+	"pause_game",
+]
